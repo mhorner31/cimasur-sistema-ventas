8479a7c4218bc8e4d9bf45cee79c917bf70dfae4
Cesar Martinez <cesarfabian_dante@outlook.com>
2017-10-25 21:48:08 -0500

    [CFM] Added Admin-Pro folder

---
+++ admin-pro/admin-angular-new/src/app/pages/icons/fontawesome/fontawesome.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+@Component({
+	templateUrl: 'fontawesome.component.html',
+	styleUrls: ['../icon.component.css'] 
+})
+export class fontawesomeComponent {
+	
+}
---
+++ admin-pro/admin-angular-new/src/app/pages/icons/material/material.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+@Component({
+	templateUrl: 'material.component.html',
+	styleUrls: ['../icon.component.css']  
+})
+export class MaterialComponent {
+	
+}
