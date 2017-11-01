856848e64d5451a5223e741537077a50b1ad9402
Donovan Rincon <rincondonovan@gmail.com>
2017-10-31 19:14:16 -0600

    [DRC] Added list of inmuebles and view for add/modify inmuebles

--- application/controllers/Inmuebles.php
+++ application/controllers/Inmuebles.php
@@ -1,21 +1,39 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<? if (! defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
  
-class Inmuebles extends CI_Controller {
+class Inmuebles extends CI_Controller
+{
      
-
     /**
     * Get All Data from this method.
     *
     * @return Response
    */
-   public function __construct() {
-    //load database in autoload libraries 
-      parent::__construct(); 
-      $this->load->model('InmueblesModel');         
-   }
+    public function __construct()
+    {
+      //load database in autoload libraries
+        parent::__construct();
+        $this->load->model('InmueblesModel');
+    }
 
 
-    public function index() 
+    public function index()
+    {
+        $inmuebles=new InmueblesModel;
+        $data['data']=$inmuebles->getInmuebles();
+
+        $this->load->helper('url');
+        $this->load->helper('html');
+
+        $this->load->view('header_app');
+        $this->load->view('topbar_app');
+        $this->load->view('sidebar_app');
+        $this->load->view('inmuebles', $data);
+        $this->load->view('footer_app');
+    }
+
+    public function update()
     {
 
         $inmuebles=new InmueblesModel;
@@ -25,11 +43,11 @@ class Inmuebles extends CI_Controller {
         $this->load->helper('url');
         $this->load->helper('html');
 
-
         $this->load->view('header_app');
         $this->load->view('topbar_app');
         $this->load->view('sidebar_app');
-        $this->load->view('inmuebles', $data);
+        $this->load->view('inmuebles_modify', $data);
         $this->load->view('footer_app');
     }
 }
+?>
--- application/views/inmuebles.php
+++ application/views/inmuebles.php
@@ -1,71 +1,64 @@
-<!-- ============================================================== -->
-<!-- Page wrapper  -->
-<!-- ============================================================== -->
 <div class="page-wrapper">
+
+<!-- ============================================================== -->
+<!-- Container fluid  -->
+<!-- ============================================================== -->
+<div class="container-fluid">
     <!-- ============================================================== -->
-    <!-- Container fluid  -->
+    <!-- Start Page Content -->
     <!-- ============================================================== -->
-    <div class="container-fluid">
-        <div class="row page-titles">
-            <div class="col-md-5 align-self-center">
-                <h3 class="text-themecolor">Alta de Inmueble</h3>
-            </div>
-        </div>
-        <!-- .row -->
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Inmuebles</h4>
-                        <h6 class="card-subtitle">Alta de inmuebles con descripción, precio y disponibilidad. </h6>
-                        
-                        <form class="form">
-                            <div class="form-group m-t-40 row">
-                                <label for="nombre" class="col-2 col-form-label">Nombre del Inmueble</label>
-                                <div class="col-10">
-                                    <input class="form-control" type="text" value="" id="nombre" required data-validation-required-message="Nombre de inmueble inválido.">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="tipo" class="col-2 col-form-label">Tipo de Inmueble</label>
-                               
-                                <div class="col-10">
-                                        <select class="custom-select col-12" id="tipo">
-                                            <option selected="">Selecciona una opción</option>
-                                            <? foreach ($tipoInmueble as $d) { ?>      
-                                                 <option value="<? $d->id ?>"><? echo $d->Nombre ?></option>      
-                                             <? }?>
-                                        </select>
-                                </div>
-                            </div>
-                            <div class="form-group row validate">
-                                <label for="precio" class="col-2 col-form-label">Precio del Inmueble</label>
-                                <div class="col-10">
-                                <div class="input-group">
-                                    <input type="number" min="0" value="0" name="onlyNum" class="form-control" required="" data-validation-required-message="Campo requerido. Verifica que está correcto el valor." aria-invalid="false"> </div>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="disponibilidad" class="col-2 col-form-label">Disponibilidad</label>
-                                <div class="col-10">
-                                    <select class="custom-select col-12" id="disponibilidad">
-                                        <option selected="">Selecciona una opción</option>
-                                        <? foreach ($dispInmueble as $d) { ?>      
-                                            <option value="<? $d->id ?>"><? echo $d->Nombre ?></option>      
-                                        <? }?>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="descripcion" class="col-2 col-form-label">Descripción</label>
-                                <div class="col-10">
-                                    <textarea class="form-control" value="" id="descripcion" required data-validation-required-message="Descripción de inmueble inválido."></textarea>
-                                </div>
-                            </div>                    
-                        </form>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Inmuebles</h4>
+                    <h6 class="card-subtitle">Exportar los inmuebles a CSV, Excel, PDF o Imprmirlos</h6>
+                    <div class="table-responsive m-t-40">
+                        <table id="example23" class="display nowrap table table-hover table-striped table-bordered" cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Tipo</th>
+                                    <th>Disponibilidad</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody>                            
+                            <? foreach($data as $d) { ?>
+                                <tr>
+                                    <th><?echo $d->Nombre?></th>
+                                    <th><?echo $d->Tipo?></th>
+                                    <th><?echo $d->Disponibilidad?></th>
+                                    <th><?echo $d->Precio?></th>
+                                </tr>
+                            <?}?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Tipo</th>
+                                    <th>Disponibilidad</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </tfoot>
+                        </table>
                     </div>
                 </div>
-            </div>
+            </div>         
         </div>
     </div>
+    <!-- ============================================================== -->
+    <!-- End PAge Content -->
+    <!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- footer -->
+<!-- ============================================================== -->
+<footer class="footer"> © 2017 Admin Pro by wrappixel.com </footer>
+<!-- ============================================================== -->
+<!-- End footer -->
+<!-- ============================================================== -->
 </div>
