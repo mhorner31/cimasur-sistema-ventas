dfb223e9e7a974e0620129cc7762e025575e7c80
Cesar Fabian Martinez Macias <cesarfabian_dante@outlook.com>
2017-10-31 18:42:16 -0600

    Primeros Cambios

---
+++ application/controllers/Citas.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Citas extends CI_Controller {
+
+    public function __construct()
+    {
+            parent::__construct();
+            $this->load->model('citas_model');
+            $this->load->helper('url_helper');
+    }
+     
+    public function index() 
+    {
+        $this->load->helper('url');
+        $this->load->helper('html');
+
+
+        $this->load->view('header_app');
+        $this->load->view('topbar_app');
+        $this->load->view('sidebar_app');
+        $this->load->view('citas');
+        $this->load->view('footer_app');
+    }
+
+    public function insert_cita() {
+        
+    }
+}
---
+++ application/models/citas_model.php
@@ -0,0 +1,8 @@
+<?php
+
+class CitasModel extends CI_Model {
+
+    public function insertCita() {
+        
+    }
+}
