4d451aae21ad234104ede05e2b00bac34429b582
M.A <marcos.anguiano@gmail.com>
2017-11-07 18:37:21 -0600

    [M.E.A.L 07-Nov-2017]
    traduccion de datatables

--- application/views/Plantilla/footer_app.php
+++ application/views/Plantilla/footer_app.php
@@ -67,6 +67,30 @@
     <script src="<?echo base_url();?>assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
     <script>
         $('#inmuebles').DataTable({
+            "language": {
+            "decimal":        "",
+            "emptyTable":     "No hay datos disponibles",
+            "info":           "Mostrando _START_ a _END_ de _TOTAL_ registro.",
+            "infoEmpty":      "Mostrando 0 a 0 de 0 registros",
+            "infoFiltered":   "(filtrado por _MAX_ registros totales)",
+            "infoPostFix":    "",
+            "thousands":      ",",
+            "lengthMenu":     "Mostrar _MENU_ registros",
+            "loadingRecords": "Cargando...",
+            "processing":     "Procesando...",
+            "search":         "Buscar:",
+            "zeroRecords":    "No se encontraron registros",
+            "paginate": {
+            "first":      "Inicio",
+            "last":       "Fin",
+            "next":       "Siguiente",
+            "previous":   "Anterior"
+        },
+        "aria": {
+            "sortAscending":  ": activar para ordenar de forma ascendente",
+            "sortDescending": ": activar para ordenar de forma descendente"
+        }
+    },
             dom: 'Bfrtip',
             buttons: [
                 'excel', 'pdf', 'print'
--- application/views/Plantilla/sidebar_app.php
+++ application/views/Plantilla/sidebar_app.php
@@ -15,8 +15,7 @@
                             </ul>
                         </li>
                         <li class="nav-devider"></li>
-                        <li class="nav-small-cap">ADMINISTRADOR</li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-gauge"></i><span class="hide-menu">Principal</span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Principal</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="index.html">Clientes </a></li>
                                 <li><a href="index.php/Inmuebles/inmuebles">Inmuebles</a></li>
@@ -24,37 +23,46 @@
                                 <li><a href="index4.html">Reportes</a></li>
                             </ul>
                         </li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-bullseye"></i><span class="hide-menu">Aplicación</span></a>
+                        <li class="nav-small-cap">ADMINISTRADOR</li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-laptop-chromebook"></i><span class="hide-menu">Sistema</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="app-calendar.html">Usuarios</a></li>
-                                <li><a href="app-chat.html">Catalogos</a></li>
+                                <li><a href="app-chat.html">Clientes</a></li>
+                                <li><a href="app-chat.html">Inmuebles</a></li>
+                                <li><a href="app-chat.html">Citas</a></li>
+                                <li><a href="app-chat.html">Medios de Publcidad</a></li>
                                 <li><a href="app-ticket.html">Base de Datos</a></li>
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-file-chart"></i><span class="hide-menu">Medios de Publicidad</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="chart-morris.html">Lista de Medios</a></li>
+                                <li><a href="chart-chartist.html">Agregar Medios</a></li>
                             </ul>
                         </li>
                         <li class="nav-small-cap">VENDEDORES</li>
                         <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-file"></i><span class="hide-menu">Clientes</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="form-basic.html">Buscar Clientes</a></li>
+                                <li><a href="form-basic.html">Lista de Clientes</a></li>
                                 <li><a href="form-layout.html">Agregar Clientes</a></li>
                             </ul>
                         </li>
                         <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-table"></i><span class="hide-menu">Citas</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="<?php echo base_url();?>index.php/citas">Citas Pendientes</a></li>
-                                <li><a href="<?php echo base_url();?>index.php/citas">Citas Concluidas</a></li>
+                                <li><a href="<?php echo base_url();?>index.php/citas">Lista de Citas</a></li>
                                 <li><a href="<?php echo base_url();?>index.php/citas/update">Agregar Cita</a></li>
                             </ul>
                         </li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-book-open-variant"></i><span class="hide-menu">Inmuebles</span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-home-variant"></i><span class="hide-menu">Inmuebles</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="<?php echo base_url();?>index.php/inmuebles">Catálogo</a></li>
-                                <li><a href="<?php echo base_url();?>index.php/inmuebles/update">Agregar Inmueble</a></li>
+                                <li><a href="<?php echo base_url();?>index.php/inmuebles/update">Agregar Inmueble ADMON</a></li>
                             </ul>
                         </li>
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-file-chart"></i><span class="hide-menu">Reportes</span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-book-open-variant"></i><span class="hide-menu">Guia de Usuario</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="chart-morris.html">Reporte Mensual</a></li>
-                                <li><a href="chart-chartist.html">Generar Reporte</a></li>
+                                <li><a href="<?php echo base_url();?>index.php/inmuebles">Guia 1</a></li>
+                                <li><a href="<?php echo base_url();?>index.php/inmuebles/update">Guia 2</a></li>
                             </ul>
                         </li>
                     </ul>
