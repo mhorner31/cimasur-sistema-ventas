a6202507116309ec7c62d0dd149909eaa4b099c4
M.A <marcos.anguiano@gmail.com>
2017-11-16 07:44:17 -0600

    session data info handling

--- application/controllers/construccion.php
+++ application/controllers/construccion.php
@@ -7,13 +7,12 @@ class Construccion extends CI_Controller {
         $this->load->helper('url');
         $this->load->helper('html');
 
-        $this->load->library('session');
 
-        $data['nickname']=$this->session->userdata('nickname');
-        $data['tipo']=$this->session->userdata('tipo');
-        $data['nombre']=$this->session->userdata('nombre');
-        $data['apellidos']=$this->session->userdata('apellidos');
-        $data['email']=$this->session->userdata('email');
+        //$data['nickname']=$this->session->userdata('nickname');
+        //$data['tipo']=$this->session->userdata('tipo');
+        //$data['nombre']=$this->session->userdata('nombre');
+        //$data['apellidos']=$this->session->userdata('apellidos');
+        $data['session_info']= getSessionData();
 
        // $data['first_name'] = $this->session->userdata('first_name');
 
@@ -22,5 +21,31 @@ class Construccion extends CI_Controller {
         $this->load->view('Plantilla/sidebar_app', $data);
         $this->load->view('Plantilla/construccion_app', $data);
         $this->load->view('Plantilla/footer_app');
+    }
+
+    public function checkSession(){
+        $this->load->helper('url');
+
+        $this->load->library('session');
+
+        if(!$this->session->userdata('nickname')){
+            redirect('login');
+        }   
+    }
+
+    public function getSessionData(){
+        $session_info;
+
+        if($this->session->userdata('nickname')){
+            $session_info = array(
+                'nickname' => $this->session->userdata('nickname'),
+                'tipo' => $this->session->userdata('tipo'),
+                'nombre' => $this->session->userdata('nombre'),
+                'apellidos' => $this->session->userdata('apellidos'),
+                'email' => $this->session->userdata('email'),
+                );
+        }
+
+        return $session_info;
     }
 }
